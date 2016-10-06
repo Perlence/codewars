f09b9b43863aed326208d65868dcb026aed50921
Sviatoslav Abakumov <abakumov.sviatoslav@progforce.com>
2016-10-07 00:03:13 +0300

    Solve 'Is this a triangle?'

---
+++ is_this_a_triangle.py
@@ -0,0 +1,33 @@
+"""Is this a triangle?
+
+Implement a method that accepts 3 integer values a, b, c. The method
+should return true if a triangle can be built with the sides of given
+length and false in any other case.
+
+(In this case, all triangles must have surface greater than 0 to be
+accepted).
+
+https://www.codewars.com/kata/is-this-a-triangle
+"""
+
+
+def is_triangle(a, b, c):
+    return a < b + c and b < a + c and c < a + b
+
+
+def test_is_triangle():
+    assert is_triangle(1, 2, 2)
+    assert not is_triangle(7, 2, 2)
+    assert not is_triangle(1, 2, 3)
+    assert not is_triangle(1, 3, 2)
+    assert not is_triangle(3, 1, 2)
+    assert not is_triangle(5, 1, 2)
+    assert not is_triangle(1, 2, 5)
+    assert not is_triangle(2, 5, 1)
+    assert is_triangle(4, 2, 3)
+    assert is_triangle(5, 1, 5)
+    assert is_triangle(2, 2, 2)
+    assert not is_triangle(-1, 2, 3)
+    assert not is_triangle(1, -2, 3)
+    assert not is_triangle(1, 2, -3)
+    assert not is_triangle(0, 2, 3)
