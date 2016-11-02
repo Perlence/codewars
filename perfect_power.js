7b6e2806bdb150ae30f7c0ad99e24935b7de1c3e
Sviatoslav Abakumov <abakumov.sviatoslav@progforce.com>
2016-11-02 22:45:01 +0300

    Solve "What's a Perfect Power anyway?"

--- perfect_power.js
+++ perfect_power.js
@@ -6,32 +6,29 @@
 
 function isPP (n) {
   for (let k = 2; k <= Math.log2(n); k++) {
-    let min = Infinity;
     for (let m of divisors(n)) {
-      let e = Math.pow(m, k);
-      min = Math.min(e, min);
-      if (e === n) {
+      if (Math.pow(m, k) === n) {
         return [m, k];
       }
-    }
-    if (min > n) {
-      break;
     }
   }
   return null;
 }
 
-function divisors (n) {
-  let result = [];
-  for (let m = 2; m <= Math.floor(n / 2); m++) {
+function * divisors (n) {
+  for (let m = 2; m <= Math.sqrt(n); m++) {
     if (n % m === 0) {
-      result.push(m);
+      yield m;
     }
   }
-  return result;
 }
 
-const test = require('tape');
+const test = require('ava');
+
+test('divisors', (t) => {
+  t.deepEqual(Array.from(divisors(36)), [2, 3, 4, 6]);
+  t.deepEqual(Array.from(divisors(144)), [2, 3, 4, 6, 8, 9, 12]);
+});
 
 test('isPP', (t) => {
   t.deepEqual(isPP(4), [2, 2], '4 = 2^2');
@@ -39,11 +36,11 @@ test('isPP', (t) => {
 
   t.is(isPP(5), null, "5 isn't a perfect number");
   t.is(isPP(28), null, "28 isn't a perfect number");
-  t.is(isPP(1000001), null, "1000001 isn't a perfect number");
+  t.is(isPP(1000000001), null, "1000001 isn't a perfect number");
 
   let pps = [4, 8, 9, 16, 25, 27, 32, 36, 49, 64, 81, 100, 121, 125, 128, 144, 169, 196, 216, 225, 243, 256, 289, 324, 343, 361, 400, 441, 484];
   for (let pp of pps) {
-    t.isNot(isPP(pp), null, `the number ${pp} is a perfect power`);
+    t.not(isPP(pp), null, `the number ${pp} is a perfect power`);
   }
 
   for (let i = 0; i < 100; ++i) {
@@ -52,7 +49,7 @@ test('isPP', (t) => {
     let l = Math.pow(m, k);
     let r = isPP(l);
     if (r === null) {
-      t.isNot(r, null, l + ' is a perfect power');
+      t.not(r, null, l + ' is a perfect power');
       break;
     } else if (Math.pow(r[0], r[1]) !== l) {
       t.is(Math.pow(r[0], r[1]), l, 'your pair (' + r[0] + ', ' + r[1] + ") doesn't work for " + l);
@@ -68,6 +65,4 @@ test('isPP', (t) => {
       break;
     }
   }
-
-  t.end();
 });
