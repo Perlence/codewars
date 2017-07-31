1bc75afaee2fcbe7745092240add90c39ed4b60c
Sviatoslav Abakumov <abakumov.sviatoslav@progforce.com>
2017-07-31 17:40:49 +0300

    Racket: Use 'andmap' and 'ormap' procedures

--- regex_password_generation.rkt
+++ regex_password_generation.rkt
@@ -7,20 +7,13 @@ https://www.codewars.com/kata/regex-password-validation
 
 (require racket/string)
 
-(define (validate-racket password)
-  (and (>= (string-length password) 6)
-       (for/and ([ch password]) (char-alphanumeric? ch))
-       (for/or ([ch password]) (char-lower-case? ch))
-       (for/or ([ch password]) (char-upper-case? ch))
-       (for/or ([ch password]) (char-numeric? ch))))
-
 (define (validate password)
   (define password-chars (string->list password))
   (and (>= (string-length password) 6)
-       (null? (filter (lambda (c) (not (char-alphanumeric? c))) password-chars))
-       (not (null? (filter char-lower-case? password-chars)))
-       (not (null? (filter char-upper-case? password-chars)))
-       (not (null? (filter char-numeric? password-chars)))))
+       (andmap char-alphanumeric? password-chars)
+       (ormap char-lower-case? password-chars)
+       (ormap char-upper-case? password-chars)
+       (ormap char-numeric? password-chars)))
 
 (define (char-alphanumeric? c)
   (or (char-lower-case? c) (char-upper-case? c) (char-numeric? c)))
